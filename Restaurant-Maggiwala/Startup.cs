c604214fcd6370bb965fe8d8a174f9b771515891
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-11-21 20:43:45 +0000

    Add project files.

---
+++ Restaurant-Maggiwala/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Restaurant_Maggiwala.Startup))]
+namespace Restaurant_Maggiwala
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
